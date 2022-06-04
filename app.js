f741644f878f62c54933d20fb4188f6884661142
Esteban Carvallo <crewxart@gmail.com>
2022-06-04 15:56:26 -0400

    timeconnection update

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const connection = mysql.createConnection({
     host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user:'bsale_test',
     password:'bsale_test',
-    database:'bsale_test'
+    database:'bsale_test',
+    connectTimeout: config.mysql.timeout //1000000
 });
 
 // Check connect
