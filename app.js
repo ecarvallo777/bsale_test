2a66c4728b02c64578bf39977d049cb843a67f95
Esteban Carvallo <crewxart@gmail.com>
2022-06-04 16:04:52 -0400

    poolconnection

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3050;
 const app = express();
 app.use(bodyParser.json());
+
 // MySql
-const connection = mysql.createConnection({
+
+const connection = mysql.createPool({
+    connectionLimit : 100, //important
     host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user:'bsale_test',
     password:'bsale_test',
     database:'bsale_test',
-    connectTimeout: config.mysql.timeout //1000000
+    debug    :  false
 });
 
-// Check connect
-connection.connect(error => {
-    if(error) throw error;
-    console.log('Data rdy!!!');
-});
+
+
 
 app.listen(PORT, () =>console.log(`Server running on port ${PORT}`));
 
