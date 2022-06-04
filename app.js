e02a36083ebf6dc7dc71620cae741026a6e2e976
Esteban Carvallo <crewxart@gmail.com>
2022-06-04 15:00:52 -0400

    upload basic files

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3050;
+const app = express();
+app.use(bodyParser.json());
+// MySql
+const connection = mysql.createConnection({
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    user:'bsale_test',
+    password:'bsale_test',
+    database:'bsale_test'
+});
+
+// Check connect
+connection.connect(error => {
+    if(error) throw error;
+    console.log('Data rdy!!!');
+});
+
+app.listen(PORT, () =>console.log(`Server running on port ${PORT}`));
+
+// Endpoints
+
+app.get('/products', (req, res)=>{
+    const sql = 'SELECT * FROM product';
+    connection.query(sql, (error, results) =>{
+        if (error) throw error;
+        if (results.length > 0){
+            res.json(results);
+        }else{res.send('Not results');
+    }
+
+        
+    });
+
+});
